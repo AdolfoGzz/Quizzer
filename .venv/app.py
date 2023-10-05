9ceee64ad6d16ed3e26d978c7647990825967cfd
AdolfoGzz <118020837+AdolfoGzz@users.noreply.github.com>
2023-10-05 10:17:32 -0600

    edits

--- .venv/app.py
+++ .venv/app.py
@@ -2,15 +2,14 @@ import streamlit as st
 import openai
 import PyPDF2 as pdf
 
-apiKey="sk-<your-key>"
-def hello():
-    st.write("Hello there")
+apiKey=st.text_input("Enter your OpenAI key", "sk-")
+#apiKey="sk-<your-key>"
 
 st.title("Quiz creation app")
 st.write("This is a quiz creation app. You can create a quiz based on a pdf file, and choose how many questions.")
 
 uploaded_file = st.file_uploader("Upload a file", type=("pdf"))
-question=st.slider("Select the number of questions", 1, 20, 10)
+question=st.slider("Select the number of questions", 1, 30, 10)
 
 if st.button("Create quiz"):
     try:
@@ -31,19 +30,19 @@ if st.button("Create quiz"):
         messages=[
         {
         "role": "system",
-        "content": "When I provide you a text and an amount of questions, I need you to create a quiz, in the same language as the text, based on the text with the amount of questions I gave. Add the correct answers at the end of the quiz."
+        "content": "When I provide you a text and an amount of questions at the end, I need you to create a 4 choice quiz, in the same language as the text, based on the text with the amount of questions I gave. Add the correct answers at the end of the quiz."
         },
         {
         "role": "user",
-        "content": f"{completeText}\n\nQuestions: {question}\n\n"
+        "content": f"{completeText}\n\nQuestions: {question}"
         },
         
         ],
-        max_tokens=4096,
+        max_tokens=8192,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0
         )
-
+        
         print(response.choices[0])
         st.write(response.choices[0].message.content)
